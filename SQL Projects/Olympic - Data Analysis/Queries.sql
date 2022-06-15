7dd7109fe6e91ec613429080d0f55a018de8bbf0
Senthil Kumar Kanagaraj <senthilkumark.nitt@gmail.com>
2022-06-15 21:30:06 +0530

    Added Query 14, 15, 16

--- SQL Projects/Olympic - Data Analysis/Queries.sql
+++ SQL Projects/Olympic - Data Analysis/Queries.sql
@@ -93,17 +93,17 @@ select * from olympics_history_noc_regions
 
 with tot_games as
               (select count(distinct games) as total_games
-              from olympics_history),
+               from olympics_history),
      countries as
               (select games, nr.region as country
-              from olympics_history oh
-              join olympics_history_noc_regions nr ON nr.noc=oh.noc
-              group by games, nr.region
-			  order by games),
+               from olympics_history oh
+               join olympics_history_noc_regions nr ON nr.noc=oh.noc
+               group by games, nr.region
+			   order by games),
      countries_participated as
               (select country, count(1) as total_participated_games
-              from countries
-              group by country)
+               from countries
+               group by country)
       select cp.*
       from countries_participated cp
       join tot_games tg on tg.total_games = cp.total_participated_games
@@ -141,7 +141,6 @@ on t3.no_of_games = t1.total_summer_games;
 	   Problem Statement: Using SQL query, Identify the sport which were just played once in all of olympics. */
 1. List out the sport played in all the games
 2. Count the occurance of each events across the years
-3. Print the 
 
 with t1 as
 (select sport,
@@ -184,6 +183,7 @@ with t1 as
 select * from olympics_history;
 
 1. Get the details of athlets who won the Gold medal
+2. Get the oldest athlets who won the Gold Medal
 
 with t1 as
 (select name,
@@ -210,8 +210,10 @@ select *
 from ranking
 where rnk = 1;
 
+/* Q10. Find the Ratio of male and female athletes participated in all olympic games. 
+		Problem Statement: Write a SQL query to get the ratio of male and female participants */
 
-/* Q10. Find the Ratio of male and female athletes participated in all olympic games. */
+
 
 /* Q11. Fetch the top 5 athletes who have won the most gold medals.  */
 select * from olympics_history
@@ -234,12 +236,49 @@ t2 as
 )
 select * from t2 where ranking < 6;
 
-
 /* Q12. Fetch the top 5 athletes who have won the most medals (gold/silver/bronze).  
-		Problem Statement: SQL Query to fetch the top 5 athletes who have won the most medals (Medals include gold, silver and bronze).*/
+		Problem Statement: SQL Query to fetch the top 5 athletes who have won the most medals (Medals include gold, silver and bronze). */
+
+1. List the athlets who won the gold/Silver/Bronze medals
+2. Apply Ranking
+3. Print the top 5
+
+with t1 as
+(select name,
+	   team,
+	   count(medal) total_medal
+from olympics_history
+where medal = 'Gold' or medal ='Silver' or medal = 'Bronze'
+group by name, team
+order by total_medal desc),
+t2 as
+(select * , dense_rank() over( order by total_medal desc) ranking
+ from t1
+)
+select * from t2 where ranking < 6;
 
 /* Q13. Fetch the top 5 most successful countries in olympics. Success is defined by no of medals won.
 		Problem Statement: Write a SQL query to fetch the top 5 most successful countries in olympics. (Success is defined by no of medals won) */
+
+1. Count the medals based on the NOC
+2. Match the NOC with region
+3. Display the top 5 countries
+
+with t1 as
+(select noc,
+	   count(medal) total_medals,
+ 	   dense_rank() over(order by count(medal) desc) as ranking
+ from olympics_history
+ where medal <> 'NA'
+ group by noc)
+select rn.region,
+	   total_medals,
+	   ranking
+from t1
+join olympics_history_noc_regions rn
+on t1.noc = rn.noc
+where ranking < 6
+order by total_medals desc;
 
 /* Q14. List down total gold, silver and bronze medals won by each country.
 		Problem Statement: Write a SQL query to list down the  total gold, silver and bronze medals won by each country.  */
@@ -285,14 +324,153 @@ order by gold desc, silver desc, bronze desc;
 /* Q15. List down total gold, silver and bronze medals won by each country corresponding to each olympic games.
 		Problem Statement: Write a SQL query to list down the  total gold, silver and bronze medals won by each country corresponding to each olympic games.  */
 
+1. List the countries which won the medals
+2. Find the total medals won by countries in each category
+3. Merge the output with Region to get the country name
+4. Pivot the output
+5. Display the games and country and disply medal details respective to the countries
+
+select oh.games,
+	   nr.region as country,
+	   oh.medal,
+	   count(1) as total_medals
+	   from olympics_history oh
+	   join olympics_history_noc_regions nr
+	   on nr.noc = oh.noc
+	   where oh.medal <> 'NA'
+	   group by oh.games, nr.region, oh.medal
+	   order by oh.games
+
+CREATE EXTENSION tablefunc;
+
+select
+	   substring(games, 1, position('-' in games) -1 ) as games,
+	   substring(games, position('-' in games) + 2) as country,
+	   coalesce(gold,0) as gold,
+	   coalesce(silver,0) as silver,
+	   coalesce(bronze,0) as bronze
+from crosstab
+		('select concat(oh.games, '' - '', nr.region) as games,
+	   	  		 oh.medal, 
+		 		 count(1) as total_medals
+		  from olympics_history oh
+		  join olympics_history_noc_regions nr
+		  on nr.noc = oh.noc
+		  where oh.medal <> ''NA''
+		  group by oh.games, nr.region, oh.medal
+		  order by oh.games',
+		  'values(''Bronze''), (''Gold''), (''Silver'')')
+		  as result(games varchar,
+				    bronze bigint,
+					gold bigint,
+					silver bigint)
+order by games;
+
+SELECT substring(games,1,position(' - ' in games) - 1) as games
+        , substring(games,position(' - ' in games) + 3) as country
+        , coalesce(gold, 0) as gold
+        , coalesce(silver, 0) as silver
+        , coalesce(bronze, 0) as bronze
+    FROM CROSSTAB('SELECT concat(games, '' - '', nr.region) as games
+                , medal
+                , count(1) as total_medals
+                FROM olympics_history oh
+                JOIN olympics_history_noc_regions nr ON nr.noc = oh.noc
+                where medal <> ''NA''
+                GROUP BY games,nr.region,medal
+                order BY games,medal',
+            'values (''Bronze''), (''Gold''), (''Silver'')')
+    AS FINAL_RESULT(games text, bronze bigint, gold bigint, silver bigint);
+
 /* Q16. Identify which country won the most gold, most silver and most bronze medals in each olympic games.
 		Problem Statement: Write SQL query to display for each Olympic Games, which country won the highest gold, silver and bronze medals.  */
 
-
+with temp as
+(select
+	   substring(games, 1, position('-' in games) -1 ) as games,
+	   substring(games, position('-' in games) + 2) as country,
+	   coalesce(gold,0) as gold,
+	   coalesce(silver,0) as silver,
+	   coalesce(bronze,0) as bronze
+ from crosstab
+		('select concat(oh.games, '' - '', nr.region) as games,
+	   	  		 oh.medal, 
+		 		 count(1) as total_medals
+		  from olympics_history oh
+		  join olympics_history_noc_regions nr
+		  on nr.noc = oh.noc
+		  where oh.medal <> ''NA''
+		  group by oh.games, nr.region, oh.medal
+		  order by oh.games, nr.region, oh.medal',
+		  'values(''Bronze''), (''Gold''), (''Silver'')')
+		  as result(games varchar,
+				    bronze bigint,
+					gold bigint,
+					silver bigint)
+ order by games)
+ select distinct games,
+ 		concat(
+			   first_value(gold) over(partition by games order by gold desc), ' - ', first_value(country) over(partition by games order by gold desc)
+		      )as Gold,
+		concat(
+			   first_value(silver) over(partition by games order by silver desc), ' - ', first_value(country) over(partition by games order by silver desc)	
+			  )as Silver,
+		concat(
+			   first_value(bronze) over(partition by games order by bronze desc), ' - ', first_value(country) over(partition by games order by bronze desc)	
+			  )as Bronze
+ from temp
+ order by games;
 
 /* Q17. Identify which country won the most gold, most silver, most bronze medals and the most medals in each olympic games.
 		Problem Statement: Similar to the previous query, identify during each Olympic Games, which country won the highest gold, silver and bronze medals.
 		Along with this, identify also the country with the most medals in each olympic games.  */
+
+with temp as
+(select
+	   substring(games, 1, position('-' in games) -1 ) as games,
+	   substring(games, position('-' in games) + 2) as country,
+	   coalesce(gold,0) as gold,
+	   coalesce(silver,0) as silver,
+	   coalesce(bronze,0) as bronze
+ from crosstab
+		('select concat(oh.games, '' - '', nr.region) as games,
+	   	  		 oh.medal, 
+		 		 count(1) as total_medals
+		  from olympics_history oh
+		  join olympics_history_noc_regions nr
+		  on nr.noc = oh.noc
+		  where oh.medal <> ''NA''
+		  group by oh.games, nr.region, oh.medal
+		  order by oh.games, nr.region, oh.medal',
+		  'values(''Bronze''), (''Gold''), (''Silver'')')
+		  as result(games varchar,
+				    bronze bigint,
+					gold bigint,
+					silver bigint)
+ order by games),
+ t2 as
+ (select games,
+ 		country,
+		gold,
+		silver,
+		bronze,
+		gold + silver + bronze as Total_medals
+ from temp)
+ select distinct games,
+ 		concat(
+			   first_value(gold) over(partition by games order by gold desc), ' - ', first_value(country) over(partition by games order by gold desc)
+		      )as MAx_Gold,
+		concat(
+			   first_value(silver) over(partition by games order by silver desc), ' - ', first_value(country) over(partition by games order by silver desc)	
+			  )as Max_Silver,
+		concat(
+			   first_value(bronze) over(partition by games order by bronze desc), ' - ', first_value(country) over(partition by games order by bronze desc)	
+			  )as Max_Bronze,
+		concat(
+			   first_value(Total_medals) over(partition by games order by Total_medals desc), ' - ', first_value(country) over(partition by games order by Total_medals desc)	
+			  )as Max_Medals
+ from t2
+ order by games;
 
 /* Q18. Which countries have never won gold medal but have won silver/bronze medals?
 Problem Statement: Write a SQL Query to fetch details of countries which have won silver or bronze medal but never won a gold medal.  */
