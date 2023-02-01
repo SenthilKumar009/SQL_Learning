1ce6ac1201c7b441d0b9e96dd4d24c92f6a80f6f
Senthil Kumar Kanagaraj <senthilkumark.nitt@gmail.com>
2023-02-01 18:28:44 +0530

    Added - LeetCode Problem -262

---
+++ Puzzles/problem_solving.sql
@@ -0,0 +1,181 @@
+'''
+Date  : 01-Feb-2023
+Author: Senthil Kumar ("The Alien") Kanagaraj
+
+Platform: LeedCode
+262. Trips and Users
+Difficulty: Hard
+
+Table: Trips
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| client_id   | int      |
+| driver_id   | int      |
+| city_id     | int      |
+| status      | enum     |
+| request_at  | date     |     
++-------------+----------+
+id is the primary key for this table.
+The table holds all taxi trips. Each trip has a unique id, while client_id and driver_id are foreign keys to the users_id at the Users table.
+Status is an ENUM type of ('completed', 'cancelled_by_driver', 'cancelled_by_client').
+ 
+
+Table: Users
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| users_id    | int      |
+| banned      | enum     |
+| role        | enum     |
++-------------+----------+
+users_id is the primary key for this table.
+The table holds all users. Each user has a unique users_id, and role is an ENUM type of ('client', 'driver', 'partner').
+banned is an ENUM type of ('Yes', 'No').
+ 
+
+The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+
+Write a SQL query to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03". Round Cancellation Rate to two decimal points.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Trips table:
++----+-----------+-----------+---------+---------------------+------------+
+| id | client_id | driver_id | city_id | status              | request_at |
++----+-----------+-----------+---------+---------------------+------------+
+| 1  | 1         | 10        | 1       | completed           | 2013-10-01 |
+| 2  | 2         | 11        | 1       | cancelled_by_driver | 2013-10-01 |
+| 3  | 3         | 12        | 6       | completed           | 2013-10-01 |
+| 4  | 4         | 13        | 6       | cancelled_by_client | 2013-10-01 |
+| 5  | 1         | 10        | 1       | completed           | 2013-10-02 |
+| 6  | 2         | 11        | 6       | completed           | 2013-10-02 |
+| 7  | 3         | 12        | 6       | completed           | 2013-10-02 |
+| 8  | 2         | 12        | 12      | completed           | 2013-10-03 |
+| 9  | 3         | 10        | 12      | completed           | 2013-10-03 |
+| 10 | 4         | 13        | 12      | cancelled_by_driver | 2013-10-03 |
++----+-----------+-----------+---------+---------------------+------------+
+Users table:
++----------+--------+--------+
+| users_id | banned | role   |
++----------+--------+--------+
+| 1        | No     | client |
+| 2        | Yes    | client |
+| 3        | No     | client |
+| 4        | No     | client |
+| 10       | No     | driver |
+| 11       | No     | driver |
+| 12       | No     | driver |
+| 13       | No     | driver |
++----------+--------+--------+
+Output: 
++------------+-------------------+
+| Day        | Cancellation Rate |
++------------+-------------------+
+| 2013-10-01 | 0.33              |
+| 2013-10-02 | 0.00              |
+| 2013-10-03 | 0.50              |
++------------+-------------------+
+Explanation: 
+On 2013-10-01:
+  - There were 4 requests in total, 2 of which were canceled.
+  - However, the request with Id=2 was made by a banned client (User_Id=2), so it is ignored in the calculation.
+  - Hence there are 3 unbanned requests in total, 1 of which was canceled.
+  - The Cancellation Rate is (1 / 3) = 0.33
+On 2013-10-02:
+  - There were 3 requests in total, 0 of which were canceled.
+  - The request with Id=6 was made by a banned client, so it is ignored.
+  - Hence there are 2 unbanned requests in total, 0 of which were canceled.
+  - The Cancellation Rate is (0 / 2) = 0.00
+On 2013-10-03:
+  - There were 3 requests in total, 1 of which was canceled.
+  - The request with Id=8 was made by a banned client, so it is ignored.
+  - Hence there are 2 unbanned request in total, 1 of which were canceled.
+  - The Cancellation Rate is (1 / 2) = 0.50
+'''
+
+drop table trips;
+
+create table trips(
+	id int,
+	client_id int,
+	driver_id int, 
+	city_id int,
+	status varchar,
+	request_at date,
+	primary key (id),
+	FOREIGN KEY(client_id) REFERENCES users(users_id),
+	FOREIGN KEY(driver_id) REFERENCES users(users_id)
+);
+
+insert into trips values
+(1, 1, 10, 1, 'completed', '2013-10-01'),
+(2, 2, 11, 1, 'cancelled_by_driver', '2013-10-01'),
+(3, 3, 12, 6, 'completed', '2013-10-01'),
+(4, 4, 13, 6, 'cancelled_by_client', '2013-10-01'),
+(5, 1, 10, 1, 'completed', '2013-10-02'),
+(6, 2, 11, 6, 'completed', '2013-10-02'),
+(7, 3, 12, 6, 'completed', '2013-10-02'),
+(8, 2, 12, 12, 'completed', '2013-10-03'),
+(9, 3, 10, 12, 'completed', '2013-10-03'),
+(10, 4, 13, 12, 'cancelled_by_driver', '2013-10-03');
+
+insert into trips values(11, 1, 10, 1, 'cancelled_by_client', '2013-10-04');
+
+create table users(
+	users_id int,
+	banned varchar,
+	role varchar,
+	primary key (users_id)
+);
+
+insert into users values
+(1, 'No', 'client'),
+(2, 'Yes', 'client'),
+(3, 'No', 'client'),
+(4, 'No', 'client'),
+(10, 'No', 'driver'),
+(11, 'No', 'driver'),
+(12, 'No', 'driver'),
+(13, 'No', 'driver')
+
+select * from users;
+select * from trips;
+
+with total_trips as
+(
+	select request_at, count(request_at) daily_trip_count
+	from trips
+	where client_id not in (select users_id from users where banned = 'Yes') and 
+		  driver_id not in (select users_id from users where banned = 'Yes') and
+		  request_at >= '2013-10-01' and request_at <='2013-10-03'
+	group by request_at
+),
+total_cancel_trip as
+(
+	select request_at, 
+	count(request_at) as total_cancel_Trip
+	from trips
+	where status like 'cancelled%' and client_id not in (select users_id from users where banned = 'Yes')
+								   and driver_id not in (select users_id from users where banned = 'Yes')
+	group by request_at
+)
+select tt.request_at Day1, daily_trip_count, total_cancel_trip,
+	   case when total_cancel_Trip is not null then
+	   			 round(((total_cancel_Trip * 1.0) / (daily_trip_count * 1.0)), 2)
+		    else 0.0
+	   end Cancellation_Rate
+from total_trips tt
+left join total_cancel_trip tct
+on tt.request_at = tct.request_at
+order by tt.request_at
